d00427349aac2f0f7605e55105b8192d9670a3fd
[Rafay123Anwar] <[muhammadrafayy0@gmail.com]>
2024-12-21 14:15:00 +0500

    first commit

--- semester/settings.py
+++ semester/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5cy_wu+dqib*1pvau*3*t!2f2nb_8v5(gf+7i-x#xds5h0vl1#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','localhost','127.0.0.1','.now.sh']
 
